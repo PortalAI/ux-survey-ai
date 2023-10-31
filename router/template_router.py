db03b34604baf8b1c14ded94b3aa0bfb81522ba3
Sam He <samhenan@gmail.com>
2023-10-31 13:31:23 -0700

    unfinished

--- router/template_router.py
+++ router/template_router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from model import service, database_model
 
-from database import table_template
+from database import table_template, table_survey
 
 template_table = table_template.TemplateTable()
+business_survey_table = table_survey.BusinessSurveyTable()
 
 router = APIRouter()
 
@@ -31,32 +32,11 @@ async def create_template(request: service.CreateTemplateRequest):
 
 @router.put("/template/", response_model=service.UpdateTemplateResponse)
 async def update_template(request: service.UpdateTemplateRequest):
+    if not business_survey_table.survey_exist(request.survey_id):
+        raise HTTPException(status_code=404, detail=f"{request.survey_id=} not found")
+    business_survey_table.update_prompts()
+    return None
 
-    # verifies that all params are present in the template
-    # for message, params in {
-    #     request.system_message: request.system_message_params,
-    #     request.agent_initial_message: request.agent_initial_message_params,
-    #     request.summary_single_prompt: request.summary_single_prompt_params,
-    #     request.get_insight_prompt: request.get_insight_prompt_params,
-    # }.items():
-    #     for param in ['{' + item + '}' for item in params]:
-    #         if param not in message:
-    #             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"{param=} not in {message=}")
-
-    template_entry = database_model.Template(
-        template_id=request.template_id,
-        survey_id=request.survey_id,
-        system_message=request.system_message,
-        system_message_params=request.system_message_params,
-        agent_initial_message=request.agent_initial_message,
-        agent_initial_message_params=request.agent_initial_message_params,
-        summary_single_prompt=request.summary_single_prompt,
-        summary_single_prompt_params=request.summary_single_prompt_params,
-        get_insight_prompt=request.get_insight_prompt,
-        get_insight_prompt_params=request.get_insight_prompt_params,
-    )
-    ret = template_table.update_template(template_entry)
-    return service.UpdateTemplateResponse(**ret)
 
 
 @router.get("/template/{template_id}", response_model=service.GetTemplateResponse)
