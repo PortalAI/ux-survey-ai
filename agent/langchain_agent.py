36edc4d2b25db6949fd30fbdc603bd7714a0a18d
Sam He <samhenan@gmail.com>
2023-10-31 19:09:14 -0700

    implement model switch

--- agent/langchain_agent.py
+++ agent/langchain_agent.py
@@ -1,3 +1,5 @@
+import openai
+import time
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
@@ -9,6 +11,9 @@ from config import settings
 import logging
 
 logger = logging.getLogger(__name__)
+GPT_4 = "gpt-4"
+GPT_3_5 = "gpt-3.5-turbo"
+
 
 class LangChainAgent:
     def __init__(self,
@@ -16,6 +21,8 @@ class LangChainAgent:
         initial_message: str | None = None,
         conversation_history: str | None = None) -> None:
         memory = ConversationBufferMemory()
+        self.gpt_model = GPT_4
+        self.falback_start_timer = None
         if system_message is not None:
             memory.chat_memory.add_message(SystemMessage(content=system_message))
         if initial_message is not None:
@@ -25,14 +32,35 @@ class LangChainAgent:
             retrieved_chat_history = ChatMessageHistory(messages=retrieved_messages)
             memory = ConversationBufferMemory(chat_memory=retrieved_chat_history)
 
-        model = ChatOpenAI(temperature=0, model_name="gpt-4", openai_api_key=settings.OPENAI_API_KEY)
+        model = ChatOpenAI(temperature=0, model_name=self.gpt_model, openai_api_key=settings.OPENAI_API_KEY, request_timeout=10, max_retries=3)
         self._chain = ConversationChain(
             llm=model,
             memory=memory,
         )
+        
 
     def generate_response(self, text) -> str:
-        return self._chain.predict(input=text)
+        try:
+            if self.gpt_model == GPT_3_5 and self.falback_start_timer is not None and time.time() - self.falback_start_timer > 60:
+                self.gpt_model = GPT_4
+                self._chain.llm = ChatOpenAI(temperature=0, model_name=self.gpt_model, openai_api_key=settings.OPENAI_API_KEY, request_timeout=10, max_retries=3)            
+                logger.info("waited %s before switching to model %s", time.time() - self.falback_start_timer, self._chain.llm.model_name)
+                self.falback_start_timer = None
+            response = self._chain.predict(input=text)
+        except (openai.error.Timeout, openai.error.RateLimitError) as e:
+            logger.warning("rate limit hit for model: %s", self.gpt_model)
+            if self.gpt_model == GPT_4:
+                self.gpt_model = GPT_3_5
+                logger.warning("switching to model: %s", self.gpt_model)
+                self.falback_start_timer = time.time()
+                self._chain.llm = ChatOpenAI(temperature=0, model_name=self.gpt_model, openai_api_key=settings.OPENAI_API_KEY, request_timeout=10, max_retries=3)
+                response = self._chain.predict(input=text)
+            else:
+                logger.exception("model %s throttled", self.gpt_model)
+                return None
+        except Exception as e:
+            logging.error("error found %s, type %s", e, type(e))
+        return response
 
     def extract_chat_history_str(self) -> str:
         extracted_messages = self._chain.memory.chat_memory.messages
